5cd7af21d40023ec4475c44bc9177bf79eacabed
Charlie Pilc <pilcwc@dukes.jmu.edu>
2022-03-24 18:55:00 -0400

    added buildings; updated rooms

--- instructor_test.py
+++ instructor_test.py
@@ -15,3 +15,11 @@ def describe_a_instructor():
         def that_has_an_office():
             ernie = Instructor("Ernie", "Ernieson", "ernie@example.com", "King 123")
             assert ernie.office == "King 123"
+
+        def that_has_a_display_name_that_updates_with_fn_and_ln():
+            francis = Instructor(
+                "Francis", "Francison", "fran@example.com", "EnGeo 456"
+            )
+            assert francis.name == "Francis Francison"
+            francis.first_name = "Frankie"
+            assert francis.name == "Frankie Francison"
