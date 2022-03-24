22ed73319b028b02bb88337a2f1223d879abddeb
Charlie Pilc <pilcwc@dukes.jmu.edu>
2022-03-24 15:56:20 -0400

    add Student class

--- person.py
+++ person.py
@@ -12,6 +12,7 @@ class Person:
     common to both of these subclasses.
     """
 
-    def __init__(self, first_name, last_name):
+    def __init__(self, first_name, last_name, email_address):
         self.first_name = first_name
         self.last_name = last_name
+        self.email = email_address
