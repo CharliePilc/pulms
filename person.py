54e9668aa742a4fb8205bd07bee9d61593875f3a
Charlie Pilc <pilcwc@dukes.jmu.edu>
2022-03-23 21:56:32 -0400

    implement Person class

---
+++ person.py
@@ -0,0 +1,17 @@
+"""
+Defines the Person class that will be the base class 
+for Students and instructors
+"""
+
+
+class Person:
+    """
+    The Person class is the base class for defining all
+    people in the PLuMS environment, including Students
+    and Instructors, and captures ass of the basic properties
+    common to both of these subclasses.
+    """
+
+    def __init__(self, first_name, last_name):
+        self.first_name = first_name
+        self.last_name = last_name
