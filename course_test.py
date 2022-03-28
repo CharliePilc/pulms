dea3ee8ebde1ce2e7ed0ec7e5f7ebc899f13f16b
Charlie Pilc <pilcwc@dukes.jmu.edu>
2022-03-28 17:15:33 -0400

    add section class

--- course_test.py
+++ course_test.py
@@ -6,10 +6,10 @@ from course import Course
 
 
 def describe_a_course():
-    def that_has_a_subject_number_title():
+    def that_has_a_subject_number_and_title():
         isat252 = Course("ISAT", "252", "Programming and Problem Solving")
         assert isat252.subject == "ISAT"
         assert isat252.number == "252"
         assert isat252.title == "Programming and Problem Solving"
-        assert isat252.name == "ISAT 252"
-        assert isat252.display_name == "ISAT 252: Programming and Problem Solving"
+        assert isat252.name == "ISAT252"
+        assert isat252.display_name == "ISAT252: Programming and Problem Solving"
